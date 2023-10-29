c406d26e7bbae7a13a07dea73632e44bb6abb91e
Bekasyl <devbekasyl@gmail.com>
2023-10-29 17:55:32 +0600

    route error

--- app/api/blogs/[blogId]/publish/route.ts
+++ app/api/blogs/[blogId]/publish/route.ts
@@ -1,32 +1,35 @@
-import { db } from "@/lib/db"
-import { NextResponse } from "next/server"
+import { NextResponse } from "next/server";
 
-export async function PATCH (
-    req: Request,
-    { params } : { params: { blogId: string}}
+import { db } from "@/lib/db";
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { blogId: string } }
 ) {
-    try {
-        const authorBlog = await db.blog.findUnique({
-            where: {
-                id: params.blogId
-            }
-        })
-        
-        if(!authorBlog){
-            return new NextResponse("Not found", {status: 404})
-        }
+  try {
 
-        const publishBlog = await db.blog.update({
-            where: {
-                id: params.blogId
-            },
-            data: {
-                isPublished: true
-            }
-        })
-        return NextResponse.json(publishBlog)
-    } catch (error) {
-        console.log("[BLOG ID PUBLISH]", error)
-        return NextResponse
+    const blog = await db.blog.findUnique({
+      where: {
+        id: params.blogId,
+      },
+    });
+
+    if (!blog) {
+      return new NextResponse("Not found", { status: 404 });
     }
+
+    const publishBlog = await db.blog.update({
+      where: {
+        id: params.blogId,
+      },
+      data: {
+        isPublished: true,
+      }
+    });
+
+    return NextResponse.json(publishBlog);
+  } catch (error) {
+    console.log("[BLOG_ID_PUBLISH]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  } 
 }
--- app/api/blogs/[blogId]/unpublish/route.ts
+++ app/api/blogs/[blogId]/unpublish/route.ts
@@ -1,30 +1,35 @@
-import { db } from "@/lib/db"
-import { NextResponse } from "next/server"
+import { NextResponse } from "next/server";
 
-export async function PATCH (
-    req: Request,
-    { params } : { params  : { blogId: string}}
+import { db } from "@/lib/db";
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { blogId: string } }
 ) {
-    try {
-        const blogAuthor = await db.blog.findUnique({
-            where: {
-                id: params.blogId
-            }
-        })
-        if(!blogAuthor) {
-            return new NextResponse("Not found", {status: 404})
-        }
-        const unpublishedBlog = await db.blog.update({
-            where: {
-                id: params.blogId
-            },
-            data: {
-                isPublished: false
-            }
-        })
-        return NextResponse.json(unpublishedBlog)
-    } catch (error) {
-        console.log("[BLOG ID UNPUBLISH]", error)
-        return new NextResponse("Internal error", { status: 500 })
+  try {
+
+    const blog = await db.blog.findUnique({
+      where: {
+        id: params.blogId,
+      },
+    });
+
+    if (!blog) {
+      return new NextResponse("Not found", { status: 404 });
     }
+
+    const unpublishedBlog = await db.blog.update({
+      where: {
+        id: params.blogId,
+      },
+      data: {
+        isPublished: false,
+      }
+    });
+
+    return NextResponse.json(unpublishedBlog);
+  } catch (error) {
+    console.log("[BLOG_ID_UNPUBLISH]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  } 
 }
